88e0cc9f8ff611c420bd737243f11e86704e2205
Helene Petoukhova <helene-7@mail.ru>
2022-11-27 19:42:14 +0500

    Special welcome

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Please, enter yout name:...");
+string username = Console.ReadLine ();
+if (username == "Mari")
+{
+    Console.WriteLine ("Super!!! It's Mari!!!");
+}
+else
+{
+    Console.Write ("Salut, ");
+    Console.WriteLine (username);
+}
